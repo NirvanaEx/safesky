2b945a9a45633cbc9a7eb39c506e47ba5441c2d6
Dias <51851060+NirvanaEx@users.noreply.github.com>
2025-02-03 07:10:33 +0500

    Удалены не нужные сервисы из request_service

--- lib/config/api_routes.dart
+++ lib/config/api_routes.dart
@@ -40,18 +40,6 @@ class ApiRoutes {
   static const String changeProfileData = '${Config.apiUrl}/auth/change-profile-data';
 
 
-  // Другие роуты для данных
-  //static const String userProfile = '${Config.apiUrl}/user/profile';
-  static const String models = '${Config.apiUrl}/models';
-  static const String flightSigns = '${Config.apiUrl}/flightSigns';
-  static const String purposes = '${Config.apiUrl}/purposes';
-  static const String regions = '${Config.apiUrl}/regions';
-  static const String requests = '${Config.apiUrl}/requests';
-
-  // Роуты для управления заявками
-  static const String cancelRequest = '${Config.apiUrl}/cancel-request';
-  static const String checkRequestStatus = '${Config.apiUrl}/check-request-status';
-  static const String sendCodeAndGetStatus = '${Config.apiUrl}/send-code-and-get-status';
 
 
 
