820684d5b1993cde2c637741a36527f49bc04841
Dias <51851060+NirvanaEx@users.noreply.github.com>
2025-02-28 12:48:22 +0500

    Добавлена версия продакшена

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
 
 class Config {
-  static const String apiUrl = 'http://91.213.31.234:8898/bpla_mobile_service/api/v1/';
+  static const String apiUrl = 'http://195.158.18.149:8085/bpla_mobile_service/api/v1/';
 }
